45d2ddd2ca2581bde2f8cb13f2865c2a3c721a73
tannokosei <tannokosei>
2025-03-12 23:53:44 +0900

    Initial commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by ピットラボ on 2025/03/01.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
